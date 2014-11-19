a5dcabf105689a258f066dc441fb80b2642a6ec7
PlayMe-Martin <martin.dirollo@gmail.com>
2014-11-19 08:46:49 +0100

    Bug correction + add icon + new animations

---
+++ post_compilation_script.bash
@@ -0,0 +1,2 @@
+cp data/Icons/Strobot-icon-red.icns application.macosx64/Strobot.app/Contents/Resources/sketch.icns
+cp data/Icons/Strobot-icon-red.icns application.macosx64/Strobot.app/Contents/Resources/sketch.icns
